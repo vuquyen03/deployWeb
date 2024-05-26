38992947cd8cc2150e2df7bf25fd817ed8c16d9e
vuquyen03 <quyenvungoc1711@gmail.com>
2024-05-26 19:20:06 +0700

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: {
-      '/graphql': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: true,
-        pathRewrite: {
-          '^/graphql': '',
-        },
-      },
-    },
+    host: '0.0.0.0', // Lắng nghe trên tất cả các địa chỉ IP
+    port: process.env.PORT || 5173, // Sử dụng cổng từ biến môi trường hoặc cổng mặc định 5173
   },
   plugins: [react()],
 });
